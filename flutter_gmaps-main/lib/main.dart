1c5d7c015b27fdd8884c02f364025688b971c74f
Salmakhaled123 <112530050+Salmakhaled123@users.noreply.github.com>
2023-04-13 05:00:51 +0200

    Update main.dart
    maps

--- flutter_gmaps-main/lib/main.dart
+++ flutter_gmaps-main/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Google Maps',
+      title: ' Google Maps',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.white,
